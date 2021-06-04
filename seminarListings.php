f810eea7cf42683cc4d9a9a4bfa986a41c921c97
mvl22 <martin@lucas-smith.co.uk>
2021-06-04 18:00:00 +0100

    Model function to get lists

--- seminarListings.php
+++ seminarListings.php
@@ -15,6 +15,7 @@ class seminarListings extends frontControllerApplication
 			'administrators'		=> 'administrators',
 			'tabUlClass'			=> 'tabsflat',
 			'database'				=> 'seminarlistings',
+			'table'					=> 'lists',
 			'disableTabs'			=> true,
 			'useTemplating'			=> true,
 		);
@@ -61,13 +62,15 @@ class seminarListings extends frontControllerApplication
 			) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Settings';
 			INSERT INTO settings (id) VALUES (1);
 			
-			-- My table
-			CREATE TABLE IF NOT EXISTS `mytable` (
-			  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Automatic key',
-			  ...
-			  `updatedAt` datetime NOT NULL COMMENT 'Updated at',
+			-- Lists
+			CREATE TABLE IF NOT EXISTS `lists` (
+			  `id` int NOT NULL AUTO_INCREMENT COMMENT 'Automatic key',
+			  `name` varchar(255) NOT NULL COMMENT 'List name',
+			  `talksdotcamId` int NOT NULL COMMENT 'Talks.cam list number (see end of URL)',
+			  `moniker` varchar(50) NOT NULL COMMENT 'URL moniker',
+			  `archived` tinyint DEFAULT NULL COMMENT 'Archived?',
 			  PRIMARY KEY (`id`)
-			) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='My table';
+			) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Lists';
 		";
 	}
 	
@@ -87,6 +90,8 @@ class seminarListings extends frontControllerApplication
 	# Additional processing
 	public function main ()
 	{
+		# Get the lists
+		$this->lists = $this->getLists ();
 		
 	}
 	
@@ -99,6 +104,26 @@ class seminarListings extends frontControllerApplication
 		
 		# Show the HTML
 		echo $html;
+	}
+	
+	
+	# Function to get lists
+	private function getLists ()
+	{
+		# Get the current lists
+		$listsById = $this->databaseConnection->select ($this->settings['database'], $this->settings['table'], array (), array (), true, $orderBy = 'name');
+		
+		# Reorganise by moniker
+		$lists = application::reindex ($listsById, 'moniker', false);
+		
+		# Decorate the lists
+		foreach ($lists as $moniker => $list) {
+			$lists[$moniker]['link'] = $this->baseUrl . "/{$moniker}/";
+			$lists[$moniker]['talksdotcamUrl'] = 'https://talks.cam.ac.uk/show/index/' . $list['talksdotcamId'];
+		}
+		
+		# Return the list of lists
+		return $lists;
 	}
 	
 	
